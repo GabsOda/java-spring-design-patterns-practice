89f7e988692030cc9d0e7eacbf94d56e9feb58cf
GabsOda <biel.oda@gmail.com>
2021-10-09 22:18:17 -0300

    first commit

---
+++ src/main/java/com/gIssamu/javaspringdesignpatternspractice/JavaSpringDesignPatternsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.gIssamu.javaspringdesignpatternspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDesignPatternsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDesignPatternsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gIssamu/javaspringdesignpatternspractice/JavaSpringDesignPatternsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gIssamu.javaspringdesignpatternspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDesignPatternsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
